14e9a357da0c24a48950b916112b1b364da4ec3d
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-01-17 13:08:23 -0800

    Load home image with webpack

--- src/content.js
+++ src/content.js
@@ -1,14 +1,15 @@
 import data from './data.json';
+import izakayaImg from './izakaya.jpg';
 
 // Takes an HTML element and populates it with page content
 export default function generateContent(element) {
     // Create elements
     const header = document.createElement('h1');
-    const img = document.createElement('img');
+    const img = new Image();
     const paragraph = document.createElement('p');
     // Set their content 
     header.innerHTML = data.homepage.header;
-    img.src = data.homepage.imgsrc;
+    img.src = izakayaImg;
     img.alt =  data.homepage.imgalt;
     paragraph.innerHTML = data.homepage.paragraph;
     // Add to passed element
